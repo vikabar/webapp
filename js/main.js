b23140fb1b3080e9d1e23d3f7f0ba5a837c58beb
vikabar <viki.bar@gmail.com>
2016-03-15 14:08:43 +0200

    added form verification and some more keyboard navigation

--- js/main.js
+++ js/main.js
@@ -52,23 +52,66 @@ window.onload = function()
 		return false;
 	}
 
-	//UTILS.qs("#rep-save").onclick = saveQuickReports;
+	UTILS.qs("#rep-save").onclick = saveQuickReports;
+
+	//define required dependencies between pairs of form inputs
+	var inputs = UTILS.qsa(".settings-panel input");
+	for (var i = 0; i < inputs.length; i++)
+	{
+		inputs[i].onblur = function(ev)
+		{
+			//find the name of the current input and the name of its neighbour input
+			var currField = ev.target;
+			var otherField = UTILS.qs("#" + currField.id.substring(0, 7) + ((currField.id.endsWith("name")) ? "URL" : "name"));
+
+			//check whether we left the input empty
+			//empty input
+			if (currField.value == "")
+			{
+				//make the neighbour input not required
+				if (otherField.hasAttribute('required'))
+				{
+					otherField.removeAttribute('required');
+				}
+			}
+			//not empty input
+			else
+			{
+				//make the neighbour input required
+				if (!otherField.hasAttribute('required'))
+				{
+					otherField.setAttribute('required', 'true');
+				}			
+			}
+		};
+	}
 }
+
 
 function keyListener(ev)
 {
 	var key = ev.keyCode;
 	var tabsNames = ["quick-reports", "my-folders", "my-team-folders", "public-folders"];
-
 	var currTab = tabsNames.indexOf(document.location.hash.substring(1));
-	var tabDiff = 38 - key;
-	var newTab = currTab - tabDiff;
 
-	if ((newTab <= 3) && (newTab >= 0))
+	//ESC is pressed inside the panel of quick reports
+	if ((key == 27) && (UTILS.qs("#quick-reports-body .settings-panel #" + event.target.id)))
 	{
-		document.location.hash = "#" + tabsNames[newTab];
+		//hide panel
+		UTILS.qs("#quick-reports-body .settings-panel").classList.add("hidden");
+	}
+	else
+	{
+		var tabDiff = 38 - key;
+		var newTab = currTab - tabDiff;
+
+		if ((newTab <= 3) && (newTab >= 0))
+		{
+			document.location.hash = "#" + tabsNames[newTab];
+		}
 	}
 }
+
 
 function unselectCurrTab() 
 {
@@ -82,6 +125,7 @@ function unselectCurrTab()
 	var currTabBody = UTILS.qs("#" + currTabName + "-body");
 	currTabBody.classList.add("hidden");
 }
+
 
 function selectTab()
 {
@@ -98,62 +142,94 @@ function selectTab()
 	UTILS.qs("#" + selectedTabBody).classList.remove("hidden");
 }
 
-/*function saveQuickReports()
+
+function saveQuickReports()
 {
-	var reports = [];
-
-	//collect data from quick reports form
-	for (var i = 1; i <= 3; i++)
+	if (validateForm("quick-reports-body"))
 	{
-		var repName = UTILS.qs("#rep_0" + i + "_name").value;
+		//hide panel
+		UTILS.qs("#quick-reports-body .settings-panel").classList.add("hidden");
 
-		//check if the row is empty
-		if (repName != "")
+		var reports = [];
+
+		//collect data from quick reports form
+		for (var i = 1; i <= 3; i++)
 		{
-			//collect report row
-			var repURL = UTILS.qs("#rep_0" + i + "_URL").value;
-			var report = {value:repURL, text:repName};
-			reports.push(report);
+			var repName = UTILS.qs("#rep_0" + i + "_name").value;
+
+			//check if the row is empty
+			if (repName != "")
+			{
+				//collect report row
+				var repURL = UTILS.qs("#rep_0" + i + "_URL").value;
+				var report = {value: repURL, text: repName};
+				reports.push(report);
+			}
 		}
+
+		var combo = UTILS.qs("#quick-reports-body .saved-links");
+
+		//remove all current options from combobox
+		while (combo.firstChild)
+		{
+		    combo.removeChild(combo.firstChild);
+		}
+
+		//put new values into the combobox
+		for (var i = 0; i < reports.length; i++)
+		{
+			var optionElement = document.createElement("option");
+			optionElement.setAttribute("value", reports[i].value);
+			optionElement.innerHTML = reports[i].text;
+			combo.appendChild(optionElement);
+		}
+
+		return false;
 	}
+}
 
-	var combo = UTILS.qs("#quick-reports-body .saved-links");
-
-	//put values into the combobox
-	for (var i = 0; i < reports.length; i++)
-	{
-		combo.appendChild(reports(i));
-	}
-
-	return false;
-	
-}*/
-
-
-var doneFunction =  function(res)
-{
-	if (res.notification)
-	{
-		var notif = UTILS.qs(".notifications");
-		notif.innerHTML = res.notification;
-		notif.classList.remove("hidden");
-	}
-	
-};
 
 function readNotificationFromFile()
 {
-	var options = {done : function(xhr, res)
-						  {
-							alert("hello world!");
-						  },
-				   fail : function(xhr, err)
-				   		  {
-				   		  	alert(err);
-				   		  },
-				   type : "json"
-				  };
+	var doneFunction =  function(res)
+	{
+		if (res.notification)
+		{
+			var notif = UTILS.qs(".notifications");
+			notif.innerHTML = res.notification;
+			notif.classList.remove("hidden");
+		}
+	};
 
-	 UTILS.ajax("data/config.json", {done: doneFunction});
+	UTILS.ajax("data/config.json", {done: doneFunction});
+}
+
+
+//check the validity of the form inside the given tab
+function validateForm(tab)
+{
+	var goodInputs = UTILS.qsa("#" + tab + " input:valid");
+
+	//remove error marking from good inputs
+	for (var i = 0; i < goodInputs.length; i++)
+	{
+		goodInputs[i].classList.remove("input-error");
+	}
+
+	//check form validity
+	if (UTILS.qs("#" + tab + " form:valid"))
+	{
+		return true;
+	}
+
+	var badInputs = UTILS.qsa("#" + tab + " input:invalid");
+
+	//mark all bad inputs
+	for (var i = 0; i < badInputs.length; i++)
+	{
+		badInputs[i].classList.add("input-error");
+	}
+
+	return false;
 }
 
